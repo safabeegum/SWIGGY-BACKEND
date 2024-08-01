67e56a5faf2e7669fa90479c84e696d7550598f5
safabeegum <pasafabeegum@gmail.com>
2024-08-01 14:18:45 +0530

    Admin SignUp Completed

---
+++ models/admin.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose")
+
+const loginSchema = mongoose.Schema(
+    {
+        name:String,
+        username:String,
+        password:String
+    }
+)
+
+const loginModel = mongoose.model("logindata",loginSchema) 
+module.exports = loginModel
